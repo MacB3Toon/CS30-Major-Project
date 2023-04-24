930567d631c13e60b86307afc4de63d30710d4c1
MacB3Toon <126741808+MacB3Toon@users.noreply.github.com>
2023-04-24 15:19:52 -0600

    major project made images updated proposal

--- Proposal.md
+++ Proposal.md
@@ -1,25 +1,25 @@
-# Major project proposal
+# Major project proposal - Fruity Ninja
 
 ## Project description
 Create a version of the FRUIT NINJA game where you can slice different types of fruit!
 
 ## Needs to have list
 - Different fruits for the user to slice
-- An effect when the fruit is cut open
 - Max number of times that a fruit can drop off the screen before game over
 - Opening start page
 - Game over page with options on what to do next
 - Functioning gravity
-- Background music
+- Background music - open game art for creative commons music
 - Sound effects
 - A blade visible on the screen when the mouse is dragged
 
 ## Nice to have list
 - Different blades to choose from
+- An effect when the fruit is cut open
 - The fruits would rotate as they go across the screen 
 - Fruits would get chopped open on the angle that they were cut at 
 - Special fruits that would have an effect on the game (ex: slow motion, no bombs)
-- The ability to always have the blade out when you choose an option, not need to press down on the mouse constantly
+- The option to always have the blade out when you choose an option, not need to press down on the mouse constantly
 - Fruit splatter would be based on what angle the fruit was cut at
 - Combos
 - High score function 
--- sketch.js
+++ sketch.js
@@ -11,25 +11,77 @@
 //create fruit splatter
 //
 
-//can you call random a global variable from inside a class -> constructor?
+//can you call random a global variable from inside a class -> constructor, yes so long as the class is after the create canvas.
 
-class Fruit{
-  constructor() {
-    //needs x, y, direction, size, image
-    this.x = random(0, windowWidth);
-    this.y = random(windowHeight, windowHeight + random(100, 400));
-    this.dx = sdfsdf;
-    this.dy = asdhgdfg;
-    this.size = sadgsdg;
-  }
+let widthran = [0, windowWidth];
+let heightran = [0, windowHeight];
+let fruits = ["watermelon", "bomb", "orange", "apple", "mango", "banana", "pineapple"];
+let watermelon;
+let bomb; 
+let orange;
+let apple;
+let mango;
+let banana; 
+let pineapple;
 
+function preload(){
+  watermelon = loadImage("watermelon.jpg");
+  bomb = loadImage(""); ERROR APEAR HERE PUT IMAGE HERE
+  orange = loadImage("orange.jpg");
+  apple = loadImage("apple.jpg");
+  mango = loadImage("mago.jpg");
+  banana = loadImage("banana.jpg");
+  pineapple = loadImage("pineapple.jpg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+class Fruit{
+  constructor() {
+    this.x = random(0, windowWidth);
+    this.y = random(windowHeight, windowHeight + random(100, 400));
+    this.dx = random[widthran];
+    this.dy = random[heightran];
+    this.size = random[50, 100];
+    this.type = random[fruits];
+  }
+
+  display(){
+    //check what fruit then display it
+    if (this.type === "watermelon"){
+      image(watermelon, this.x, this.y, this.size)
+    }
+    else if (this.type === "orange"){
+      image(orange, this.x, this.y, this.size)
+    }
+    else if (this.type === "bomb"){
+      image(bomb, this.x, this.y, this.size)
+    }
+    else if (this.type === "apple"){
+      image(apple, this.x, this.y, this.size)
+    }
+    else if (this.type === "mango"){
+      image(mango, this.x, this.y, this.size)
+    }
+    else if (this.type === "banana"){
+      image(banana, this.x, this.y, this.size)
+    }
+    else if (this.type === "pineapple"){
+      image(pineapple, this.x, this.y, this.size)
+    }
+  }
+
+  gravity(){
+    //make the fruit fall back down after a certain point
+  }
+
+  sliced(){
+    //check if the mouse has hit it, if so, cut in half?
+  }
+}
+
 function draw() {
   background(220);
-  circle(mouseX, mouseY, 50, 50);
 }
